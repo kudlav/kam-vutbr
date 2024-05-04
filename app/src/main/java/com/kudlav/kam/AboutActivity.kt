63f12c7d81f0c2535e403fb108116ab3178f7c86
Vladan Kudlac <vladankudlac@gmail.com>
2024-05-05 00:17:07 +0200

    Rewrite About page

--- app/src/main/java/com/kudlav/kam/AboutActivity.kt
+++ app/src/main/java/com/kudlav/kam/AboutActivity.kt
@@ -1,7 +1,10 @@
 package com.kudlav.kam
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.kudlav.kam.databinding.ActivityAboutBinding
 
 class AboutActivity : AppCompatActivity() {
@@ -14,6 +17,35 @@ class AboutActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.tvVersion.text = getString(R.string.version, BuildConfig.VERSION_NAME)
+
+        binding.tvVersion.setOnClickListener {
+            openWebpage("https://github.com/kudlav/kam-vutbr/releases")
+        }
+
+        binding.tvGithub.setOnClickListener {
+            openWebpage("https://github.com/kudlav/kam-vutbr")
+        }
+
+        binding.tvTranslate.setOnClickListener {
+            openWebpage("https://crowdin.com/project/kam-vut")
+        }
+
+        binding.tvAuthorKudlav.setOnClickListener {
+            openWebpage("https://kudlav.github.io")
+        }
+
+        binding.tvPrivacy.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle(R.string.privacy_title)
+                .setMessage(R.string.privacy_content)
+                .setNegativeButton(R.string.label_ok) { _, _ -> }
+                .show()
+        }
+    }
+
+    private fun openWebpage(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
     }
 
 }
